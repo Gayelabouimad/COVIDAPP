ebeb501672da2f86fb58e25728ece58bf0e640e5
Gayelabouimad <gayelabouimad@outlook.com>
2020-06-08 17:53:11 +0300

    updates to deploy to heroku 2

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/covidapp'));
+app.use(express.static('./dist/covidapp'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/covidapp/index.html'));
+    res.sendFile('index.html', {root: 'dist/covidapp/'});
 });
 
 // Start the app by listening on the default Heroku port
